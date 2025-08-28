318b9aa6fbcb2e84d0f6ec6bc2caedefac43ff41
hello-sahasraka <sahasrakasachin@gmail.com>
2025-08-28 14:57:27 +0530

    feat: add logout functionality to admin dashboard
    - Implement logout button in admin dashboard header

--- opalglow-frontend/src/Components/Sidebar.jsx
+++ opalglow-frontend/src/Components/Sidebar.jsx
@@ -1,8 +1,14 @@
-import { ArrowDownLeftSquareIcon, ChartNoAxesGantt, UserRound } from "lucide-react";
-import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import { ArrowDownLeftSquareIcon, ChartNoAxesGantt, LogOut, UserRound } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const location = useLocation();
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token");
 
   const navLinks = [
     { path: "/admin/users", label: "Users", icon: <UserRound size={20} /> },
@@ -10,9 +16,24 @@ const Sidebar = () => {
     { path: "/admin/orders", label: "Orders", icon: <ArrowDownLeftSquareIcon size={20} /> },
   ];
 
+  useEffect(() => {
+    if (token) {
+      axios.get(import.meta.env.VITE_BACKEND_URL + "/api/user/currentuser", {
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+      }).then((response) => {
+        setUser(response.data);
+      }).catch((e) => {
+        console.log(e);
+        setUser(null);
+      })
+    }
+  }, [token]);
+
   return (
-    <aside className="h-screen w-full bg-gray-900 text-white shadow-md">
-      <div className="flex flex-col pt-10 px-4 gap-3">
+    <aside className="h-screen w-full bg-gray-900 text-white shadow-md flex flex-col justify-between">
+      <div className="flex flex-col justify-between border-white pt-10 px-4 gap-3">
         <h2 className="text-lg font-semibold mb-6 text-gray-200 tracking-wide px-2">Admin Panel</h2>
 
         {navLinks.map(({ path, label, icon }) => {
@@ -31,7 +52,18 @@ const Sidebar = () => {
             </Link>
           );
         })}
+
       </div>
+
+      <button className="flex gap-3 mx-4 my-12 px-4 py-2 rounded-md transition-all duration-200 text-sm font-medium bg-blue-600 hover:bg-blue-500/90 text-white"
+        onClick={() => {
+          setUser(null);
+          localStorage.removeItem("token");
+          navigate("/login");
+        }}
+      >
+        <LogOut size={20} /><span>Logout</span>
+      </button>
     </aside>
   );
 };
